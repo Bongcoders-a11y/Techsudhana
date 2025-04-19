e552719d042b55a4c3f74a4c3babc221d566f0cd
Bongcoders-a11y <rockd0128@gmail.com>
2025-04-20 03:09:25 +0530

    Delete backend directory

--- backend/routes/trending.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { getTrendingNews } from "../controllers/newsController.js"
-
-const router = express.Router()
-
-router.get("/", getTrendingNews)
-
-export default router
